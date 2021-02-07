e828f4fc2abbc8fd7f0b88a6f69a08e05cf6dcca
Chris LoJacono <clojacono92@gmail.com>
2021-02-07 14:02:02 -0600

    setting up

---
+++ src/App/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App d-flex justify-content-center">
+        <h2>Start Coding!</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
